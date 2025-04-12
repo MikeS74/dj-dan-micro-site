5bb987ea28697a9a408da1fe7a0cd214e19bb9f2
Michael Sanchez <michaelleesanchez1@gmail.com>
2025-04-12 08:26:57 -0700

    create a placeholder component for TicketLink

---
+++ app/components/TicketLink.js
@@ -0,0 +1,19 @@
+'use client'
+
+import styled from 'styled-components';
+
+const TicketLink = styled.div`
+  background-color: #0070f3;
+  color: white;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  font-size: 16px;
+
+  &:hover {
+    background-color: #005bb5;
+  }
+`;
+
+export default TicketLink;
--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import TicketLink from "./components/TicketLink";
 
 export default function Home() {
   return (
@@ -19,7 +20,7 @@ export default function Home() {
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
-
+          <TicketLink>Get Tickets</TicketLink>
         <div className={styles.ctas}>
           <a
             className={styles.primary}
