f8d2c4a08e538b81aa5e4fdaa253a424d7b7561e
Michael Sanchez <michaelleesanchez1@gmail.com>
2025-04-16 10:16:09 -0700

    add PageLayout and HeaderDesktop

---
+++ app/components/PageLayout.js
@@ -0,0 +1,12 @@
+'use client'
+
+import styled from 'styled-components';
+
+const PageLayout = styled.div`
+display: flex;
+flex-direction: column;
+padding: 16px;
+align-items: center;
+`;
+
+export default PageLayout;
--- app/page.js
+++ app/page.js
@@ -1,9 +1,12 @@
+import PageLayout from "./components/PageLayout";
+import HeaderDesktop from "./components/HeaderDesktop";
 import TicketLink from "./components/TicketLink";
 
 export default function Home() {
   return (
-    <div>
+    <PageLayout>
+      <HeaderDesktop />
        <TicketLink>Get Tickets</TicketLink>
-    </div>
+    </PageLayout>
   );
 }
