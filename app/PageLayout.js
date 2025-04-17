c8bb19c3bf7af19eb0a05f143504dc6e6529ee84
Michael Sanchez <michaelleesanchez1@gmail.com>
2025-04-17 09:13:50 -0700

    improve PageLayout

--- app/PageLayout.js
+++ app/PageLayout.js
@@ -2,24 +2,41 @@
 
 import styled from 'styled-components';
 import HeaderDesktop from './components/HeaderDesktop';
-import TicketLink from './components/TicketLink';
+import TourSchedule from './components/TourSchedule';
 import useMediaQuery from './helpers/useMediaQuery';
+
+ const mainStackGap = 64;
 
 const PageLayoutStyled = styled.div`
 display: flex;
 flex-direction: column;
 padding: 16px;
-align-items: center;
+`;
+
+const HeaderLayout = styled.div`
+margin: 16px auto ${mainStackGap}px auto;
+`;
+
+const MainContent = styled.div`
+display: flex;
+flex-direction: column;
+gap: ${mainStackGap}px;
+width: 100%;
+margin: 0 auto;
+max-width: 560px;
 `;
 
 const PageLayout = () => {
 const showMobileLayout = useMediaQuery('(max-width: 620px)');
     return (
 <PageLayoutStyled>
+    <HeaderLayout>
     {showMobileLayout ? null :  <HeaderDesktop />}
-    <TicketLink>
-        Get Tickets
-    </TicketLink>
+    </HeaderLayout>
+    <MainContent>
+    <TourSchedule />
+    {/* <div style={{ width: '100%', height: 80, backgroundColor: '#222222' }} /> */}
+    </MainContent>
 </PageLayoutStyled>
     )
 }
